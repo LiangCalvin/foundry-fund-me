b7f97343809701f119a47399f964f77eb0972e17
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2026-05-12 12:12:39 +0700

    feat update getVersion to remove hardcode priceFeed

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -36,10 +36,7 @@ contract FundMe {
     }
 
     function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
-        return priceFeed.version();
+        return s_priceFeed.version();
     }
 
     modifier onlyOwner() {
