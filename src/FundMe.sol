be79d408dea377724ec4991abe04c3ba67cb30bb
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2026-05-12 12:12:18 +0700

    feat add s_priceFeed in getconversionRate

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -27,10 +27,10 @@ contract FundMe {
 
     function fund() public payable {
         require(
-            msg.value.getConversionRate() >= MINIMUM_USD,
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
             "You need to spend more ETH!"
         );
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        // require(PriceConverter.getConversionRate(msg.value, s_priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
